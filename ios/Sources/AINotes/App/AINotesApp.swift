314b1c9165bcc4840534d2a022ab137485172579
Izaan Qaiser <i2qaiser@uwaterloo.ca>
2026-09-19 15:19:35 -0400

    Add debug onboarding replay toggle

--- ios/Sources/AINotes/App/AINotesApp.swift
+++ ios/Sources/AINotes/App/AINotesApp.swift
@@ -2,6 +2,16 @@ import SwiftUI
 
 @main
 struct AINotesApp: App {
+    init() {
+#if DEBUG
+        let defaults = UserDefaults.standard
+        if defaults.bool(forKey: "nomi.debugReplayOnboardingOnLaunch") {
+            defaults.set(false, forKey: "nomi.hasCompletedFirstLaunch")
+            defaults.removeObject(forKey: "nomi.onboardingProjectID")
+        }
+#endif
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
